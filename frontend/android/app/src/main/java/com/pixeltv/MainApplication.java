691823b6a7990e526d93761114a495944f126d1a
andicretu <cretuandi@gmail.com>
2025-07-04 15:10:31 +0200

    Not working. Even though manual linking now works, the Gradle-Plugin path (i.e. plugins { id com.facebook.react }) never generated a PackageList because the RNGP codegen plugin never actually got applied to your :app module.

--- frontend/android/app/src/main/java/com/pixeltv/MainApplication.java
+++ frontend/android/app/src/main/java/com/pixeltv/MainApplication.java
@@ -3,41 +3,42 @@ package com.pixeltv;
 import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
-import com.facebook.soloader.SoLoader;
-import com.facebook.react.PackageList;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.PackageList;                   // ← import this
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.jscexecutor.JSCExecutorFactory;
 import com.facebook.react.modules.systeminfo.AndroidInfoHelpers;
+import com.facebook.soloader.SoLoader;
 
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+    private final ReactNativeHost mReactNativeHost =
+            new ReactNativeHost(this) {
+                @Override
+                public boolean getUseDeveloperSupport() {
+                    return BuildConfig.DEBUG;
+                }
 
-    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return BuildConfig.DEBUG;
-        }
+                // ← completely replace your old getPackages() with this:
+                @Override
+                protected List<ReactPackage> getPackages() {
+                    return new PackageList(this).getPackages();
+                }
 
-        @Override
-        protected List<com.facebook.react.ReactPackage> getPackages() {
-            // Autolinked packages include WebView and others
-            return new PackageList(this).getPackages();
-        }
+                @Override
+                protected String getJSMainModuleName() {
+                    return "index";
+                }
 
-        @Override
-        protected String getJSMainModuleName() {
-            return "index";
-        }
-
-        @Override
-        protected JavaScriptExecutorFactory getJavaScriptExecutorFactory() {
-            return new JSCExecutorFactory(
-                    getApplicationContext().getPackageName(),
-                    AndroidInfoHelpers.getFriendlyDeviceName()
-            );
-        }
-    };
+                @Override
+                protected JavaScriptExecutorFactory getJavaScriptExecutorFactory() {
+                    return new JSCExecutorFactory(
+                            getApplicationContext().getPackageName(),
+                            AndroidInfoHelpers.getFriendlyDeviceName()
+                    );
+                }
+            };
 
     @Override
     public ReactNativeHost getReactNativeHost() {
