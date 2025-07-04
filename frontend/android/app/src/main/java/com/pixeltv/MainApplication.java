0875a5e6c0a1627e1354ff9aecfb5b92d33c8457
andicretu <cretuandi@gmail.com>
2025-07-04 17:33:41 +0200

    Loading but not starting

--- frontend/android/app/src/main/java/com/pixeltv/MainApplication.java
+++ frontend/android/app/src/main/java/com/pixeltv/MainApplication.java
@@ -4,49 +4,50 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.PackageList;                   // ← import this
+import com.facebook.react.shell.MainReactPackage;               // ← core package
+import com.reactnativecommunity.webview.RNCWebViewPackage;     // ← WebView
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.jscexecutor.JSCExecutorFactory;
 import com.facebook.react.modules.systeminfo.AndroidInfoHelpers;
 import com.facebook.soloader.SoLoader;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
-    private final ReactNativeHost mReactNativeHost =
-            new ReactNativeHost(this) {
-                @Override
-                public boolean getUseDeveloperSupport() {
-                    return BuildConfig.DEBUG;
-                }
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
 
-                // ← completely replace your old getPackages() with this:
-                @Override
-                protected List<ReactPackage> getPackages() {
-                    return new PackageList(this).getPackages();
-                }
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new RNCWebViewPackage()
+            );
+        }
 
-                @Override
-                protected String getJSMainModuleName() {
-                    return "index";
-                }
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
 
-                @Override
-                protected JavaScriptExecutorFactory getJavaScriptExecutorFactory() {
-                    return new JSCExecutorFactory(
-                            getApplicationContext().getPackageName(),
-                            AndroidInfoHelpers.getFriendlyDeviceName()
-                    );
-                }
-            };
+        @Override
+        protected JavaScriptExecutorFactory getJavaScriptExecutorFactory() {
+            return new JSCExecutorFactory(
+                    getApplicationContext().getPackageName(),
+                    AndroidInfoHelpers.getFriendlyDeviceName()
+            );
+        }
+    };
 
-    @Override
-    public ReactNativeHost getReactNativeHost() {
+    @Override public ReactNativeHost getReactNativeHost() {
         return mReactNativeHost;
     }
 
-    @Override
-    public void onCreate() {
+    @Override public void onCreate() {
         super.onCreate();
         SoLoader.init(this, /* native exopackage */ false);
     }
